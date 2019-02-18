e256b5470c14e6ab99b116250c9fafaea1f53b4a
Coach Bob <bkryger@gmail.com>
2019-02-17 20:47:06 -0500

    HELLO AGAIN THIS IS NEWEST CODE

--- competitionCode.py
+++ competitionCode.py
@@ -1,3 +1,4 @@
+from xlsxwriter.utility import xl_rowcol_to_cell as getCell
 import xlrd
 import xlsxwriter as write
 import csv
@@ -17,26 +18,32 @@ timestamp = re.sub(r'\..+','',timestamp)
 timestamp = str(date) + "_" + timestamp
 excelFile = re.sub(r'RawExport-.+$',timestamp,latestFileName) + ".xlsx"
 print(excelFile)
-workbook = write.Workbook('C:\\Users\\Jules\\Desktop\\' + excelFile)
+workbook = write.Workbook('C:\\Users\\Jules\\Desktop\\' + excelFile, {'strings_to_numbers':True})
 contents = workbook.add_worksheet("Teams")
 averages = workbook.add_worksheet("Avg")
-predicter = workbook.add_worksheet("CrystalBall")
+predicter = workbook.add_worksheet("Predicter")
 try:
     csv_file = open(latestFile)
     csv_reader = csv.DictReader(csv_file)
     teams = {}
-    r = 0
+    r = 1
     for row in csv_reader:
         teamNum = (row["Team Number"])
         if (teamNum not in teams.keys()):
             teams[row["Team Number"]] = workbook.add_worksheet(row["Team Number"])
-            r = 0
+            r = 1
         for c, col in enumerate(row):
             teams[row["Team Number"]].write(0, c, col)
         for ci in row.keys():
             for c, col in enumerate(row.items()):                
-                teams[row["Team Number"]].write(r+1, c, col[1])
+                teams[row["Team Number"]].write(r, c, col[1])
         r+=1
+        for x in range (2, c):
+            avgFormat = workbook.add_format()
+            avgFormat.set_bold()
+            teams[row["Team Number"]].write(r, 0, "AVERAGE",avgFormat)
+            formulaAvg = '=IF(TYPE(' + getCell(1, x) + ')=1,AVERAGE(' + getCell(1, x) + ':' + getCell(r-1, x) + '), "")'
+            teams[row["Team Number"]].write(r, x, formulaAvg,avgFormat)
 finally:
     csv_file.close()
     
