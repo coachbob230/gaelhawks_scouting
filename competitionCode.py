8c6ea5729217ac12fdaa32a7acb67b355dfec895
Coach Bob <bkryger@gmail.com>
2019-02-17 20:39:44 -0500

    first-commit

---
+++ competitionCode.py
@@ -0,0 +1,45 @@
+import xlrd
+import xlsxwriter as write
+import csv
+import statistics
+import datetime
+import glob
+import os
+import re
+
+list_of_files = glob.glob(r'C:\Users\Jules\Dropbox\Scouting 2019\*.csv')
+latestFile = max(list_of_files, key=os.path.getctime)
+latestFileName = os.path.basename(max(list_of_files, key=os.path.getctime))
+date = datetime.date.today()
+now = datetime.datetime.time(datetime.datetime.now())
+timestamp = re.sub(r':','',str(now))
+timestamp = re.sub(r'\..+','',timestamp)
+timestamp = str(date) + "_" + timestamp
+excelFile = re.sub(r'RawExport-.+$',timestamp,latestFileName) + ".xlsx"
+print(excelFile)
+workbook = write.Workbook('C:\\Users\\Jules\\Desktop\\' + excelFile)
+contents = workbook.add_worksheet("Teams")
+averages = workbook.add_worksheet("Avg")
+predicter = workbook.add_worksheet("CrystalBall")
+try:
+    csv_file = open(latestFile)
+    csv_reader = csv.DictReader(csv_file)
+    teams = {}
+    for row in csv_reader:
+        teamNum = (row["Team Number"])
+        if (teamNum not in teams.keys()):
+            teams[row["Team Number"]] = workbook.add_worksheet(row["Team Number"])
+        for c, col in enumerate(row):
+            teams[row["Team Number"]].write(1, c, col)
+finally:
+    csv_file.close()
+    
+with open (latestFile) as csv_file:
+    rawData = workbook.add_worksheet("Raw Data")
+    reader = csv.reader(csv_file)
+    for r, rows in enumerate(reader):
+        for c, col in enumerate(rows):
+            rawData.write(r, c, col)
+
+
+workbook.close()
