fc9faa123fcf55f1bd9a7a9612b3c42505de9c84
Coach Bob <bkryger@gmail.com>
2019-02-18 00:15:32 -0500

    updated a bin

--- competitionCode.py
+++ competitionCode.py
@@ -1,5 +1,4 @@
 from xlsxwriter.utility import xl_rowcol_to_cell as getCell
-import xlrd
 import xlsxwriter as write
 import csv
 import statistics
@@ -8,7 +7,22 @@ import glob
 import os
 import re
 
-list_of_files = glob.glob(r'C:\Users\Jules\Dropbox\Scouting 2019\*.csv')
+
+
+def calculate(sheet,lastrow):
+    """Add all the calculated values to the sheet"""
+    r=lastrow+1
+    avgFormat = workbook.add_format()
+    avgFormat.set_bold()
+    sheet.write(r, 0, "AVERAGE", avgFormat)
+    # Loop through each sheet and add in calculations
+    for x in range (2, col-1):
+        formulaAvg = '=IF(TYPE(' + getCell(1, x) + ')=1,AVERAGE(' + getCell(1, x) + ':' + getCell(r-1, x) + '), "")'
+        sheet.write(r, x, formulaAvg, avgFormat)
+
+
+
+list_of_files = glob.glob(r'/home/bobk/workspace/gaelhawks_scouting/*.csv')
 latestFile = max(list_of_files, key=os.path.getctime)
 latestFileName = os.path.basename(max(list_of_files, key=os.path.getctime))
 date = datetime.date.today()
@@ -18,32 +32,54 @@ timestamp = re.sub(r'\..+','',timestamp)
 timestamp = str(date) + "_" + timestamp
 excelFile = re.sub(r'RawExport-.+$',timestamp,latestFileName) + ".xlsx"
 print(excelFile)
+
 workbook = write.Workbook('C:\\Users\\Jules\\Desktop\\' + excelFile, {'strings_to_numbers':True})
 contents = workbook.add_worksheet("Teams")
 averages = workbook.add_worksheet("Avg")
 predicter = workbook.add_worksheet("Predicter")
+
 try:
     csv_file = open(latestFile)
     csv_reader = csv.DictReader(csv_file)
+
     teams = {}
     r = 1
+
+    # for each team, create a sheet and populate
     for row in csv_reader:
         teamNum = (row["Team Number"])
+        # when we find a new team number create a new sheet
         if (teamNum not in teams.keys()):
+            # first finish up the previous sheet
+            # ** Note python's crappy way to test if a variable is defined
+            try:      # test if variable is defined
+                prevteam
+            except:   # it is not defined, don't do anything
+                pass
+            else:     # it is defined, do this stuff
+                calculate(teams[prevteam],r)
+            prevteam=teamNum
+            # now create new sheet and initialize
             teams[row["Team Number"]] = workbook.add_worksheet(row["Team Number"])
             r = 1
-        for c, col in enumerate(row):
-            teams[row["Team Number"]].write(0, c, col)
-        for ci in row.keys():
-            for c, col in enumerate(row.items()):                
-                teams[row["Team Number"]].write(r, c, col[1])
+
+        col=0
+        for c, (k, v) in enumerate(row.items()):
+            # if the key starts with a space, ignore it, it is a header
+            if re.match('^ ',k): 
+                continue
+            if re.match('Team Number',k): 
+                continue
+            # Add header in top row
+            teams[row["Team Number"]].write(0, col, k)
+            # Add values to rows
+            teams[row["Team Number"]].write(r, col, v)
+            col=col+1
         r+=1
-        for x in range (2, c):
-            avgFormat = workbook.add_format()
-            avgFormat.set_bold()
-            teams[row["Team Number"]].write(r, 0, "AVERAGE",avgFormat)
-            formulaAvg = '=IF(TYPE(' + getCell(1, x) + ')=1,AVERAGE(' + getCell(1, x) + ':' + getCell(r-1, x) + '), "")'
-            teams[row["Team Number"]].write(r, x, formulaAvg,avgFormat)
+
+except Exception as ee:
+    print(ee)
+
 finally:
     csv_file.close()
     
